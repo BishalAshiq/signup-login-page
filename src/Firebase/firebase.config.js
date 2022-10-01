3ae63e10358a4917ce7fb79b0bc6631267d20a75
BishalAshiq <bishaltheboss20@gmail.com>
2022-10-01 17:48:15 +0600

    initialize firebase

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJWrN3DpbgMEUrvs3DdMg6h7NzZxhJYKc",
+    authDomain: "signup-login-authen.firebaseapp.com",
+    projectId: "signup-login-authen",
+    storageBucket: "signup-login-authen.appspot.com",
+    messagingSenderId: "655971111131",
+    appId: "1:655971111131:web:2198214881aa3358103cfe"
+  };
+  export default firebaseConfig;
